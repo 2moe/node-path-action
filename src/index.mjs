2eec9704e62a53215d7b2b5dd582cc1617de1f19
2moe <m@tmoe.me>
2024-06-06 22:40:01 +0800

    fix(gh-output): +EOL

--- src/index.mjs
+++ src/index.mjs
@@ -1,9 +1,10 @@
 import { appendFileSync } from "node:fs";
+import { EOL } from "node:os";
 import process from "node:process";
 
 const exePath = process.execPath;
 console.info(exePath);
 console.debug(
-    'You can use ${{steps."action-id".outputs.exe}} to get the output (please replace "action-id" with the specific id)',
+	'You can use ${{steps."action-id".outputs.exe}} to get the output (please replace "action-id" with the specific id)',
 );
-appendFileSync(process.env.GITHUB_OUTPUT, `exe=${exePath}`);
+appendFileSync(process.env.GITHUB_OUTPUT, `exe=${exePath}${EOL}`);
