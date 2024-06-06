2eea6f2df493d4338d49308d203a898df4185598
2moe <m@tmoe.me>
2024-06-06 21:46:40 +0800

    chore: update action.yml

---
+++ .github/switch-action-yml.zsh
@@ -0,0 +1,16 @@
+#!/usr/bin/env/zsh -f
+
+for i ({16..32..4}) {
+    ver=v${i}
+    git checkout --orphan=$ver
+    args=(
+        set action.yml
+        --key runs.using
+        --str node${i}
+        --save
+    )
+    tomlyre $args
+    git add .
+    git commit -m "chore: update action.yml"
+    git push
+}
