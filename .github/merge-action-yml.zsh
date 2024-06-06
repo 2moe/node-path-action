6d5b0c012fe5678688e2ad9ca6dc15289a6fbb29
2moe <m@tmoe.me>
2024-06-06 22:38:47 +0800

    fix(gh-output): +EOL

---
+++ .github/merge-action-yml.zsh
@@ -0,0 +1,12 @@
+#!/bin/zsh -f
+
+for i ({16..32..4}) {
+    ver=v${i}
+    git checkout $ver
+    git add .
+    git merge --squash -X theirs dev
+    git commit -m "fix(gh-output): +EOL"
+    git push --set-upstream origin $ver
+}
+
+git checkout dev
